5a29afa1198d2afa6eeb372fb5dce3a732a910c7
Ibrohim <93027235+IbrohimFayzullayev@users.noreply.github.com>
2022-03-01 23:20:34 +0500

    same js code write

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
-'use strict';
-let randomNum = Math.trunc(Math.random() * 6) + 1;
+"use strict";
+let randomNum;
+let score1 = 0;
+let scor1 = document.querySelector("#score--0").textContent;
+document.querySelector(".btn--roll").addEventListener("click", function () {
+  randomNum = Math.trunc(Math.random() * 6) + 1;
+  document.querySelector(".dice").src = `dice-${randomNum}.png`;
+  if (randomNum == 1) {
+    score1 = 0;
+    document.querySelector("#score--0").textContent = score1;
+  } else {
+    score1 += randomNum;
+    document.querySelector("#score--0").textContent = score1;
+  }
+});
+let player1;
+let player2;
+let ball = score1;
+document.querySelector(".btn--hold").addEventListener("click", function () {
+  ball += score1;
+  document.querySelector("#current--0").textContent = ball;
+  document.querySelector("#score--0").textContent = 0;
+  score1 = 0;
+});
